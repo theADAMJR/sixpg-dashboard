23f0b5f97767343ccc6dd4919d6b6a8b72e7a8d3
theADAMJR <adamjf123@gmail.com>
2020-07-11 16:10:49 +0100

    first

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  endpoint: '/api',
-  discordInvite: 'https://discord.gg/24tycau',
-  githubURL: 'https://github.com/theADAMJR/2PG-Dashboard',
-  version: 'v0.2.0b',
-  stripePublicKey: 'pk_live_kyJ00K40kgFhHMYzO7Bc9YjV00sVDW6IlV'
-};
